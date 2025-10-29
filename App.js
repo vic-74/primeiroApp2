538dd4d13ad1c3efa3fe37f20f4ac7df261c09c4
vic-74 <victormirandafernandes574@gmail.com>
2025-10-29 09:29:20 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>bem vindo ao meu App!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#214cce',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
